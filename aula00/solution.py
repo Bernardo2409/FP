a885a25d4f04112ff7a24f468109d239f4491d86
Bernardo Coelho <bernardomcoelho@ua.pt>
2025-01-21 18:57:46 +0000

    Add files via upload

--- aula00/solution.py
+++ aula00/solution.py
@@ -1,11 +1,33 @@
-# Este programa é a solução de um dos exercícios da aula.
+# Exercise 5 on "How to think like a computer scientist", ch. 11.
 
-largura = float(input('Largura? '))
-altura = float(input('Altura? '))
+import turtle
 
-area = largura * altura
-perimetro = largura*2 + altura*2
+def main():
+    screen = turtle.Screen()
+    t = turtle.Turtle()
 
-print('Area:', area)
-print('Perimetro:', perimetro)
+    # Use t.up(), t.down() and t.goto(x, y)
+    with open( 'D:\UA\1ano\FP\aula06\drawing.txt' , 'r') as fileobj:
+        for line in fileobj:
+            line = line.strip()
+            if line == "UP":
+                t.up()
+            elif line == "DOWN":
+                t.down()
+            else:
+                coords = line.split(" ")
+                x,y = int(coords[0]), int(coords[1])
+                t.goto(x,y)
+        
+
+    # Put your code here
+
+
+    # Wait until window is closed
+    screen.mainloop()
+
+
+if __name__ == "__main__":
+    main()
+
 
